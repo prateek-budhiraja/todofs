a27891557c9f6d08d52869ad529b7b64c19c8c00
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2022-12-23 18:41:50 +0530

    added cors module

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+// const cors = require("cors");
+
 // Routes
 const v1todo = require("./routes/v1todo.route");
 
@@ -13,6 +15,7 @@ dbConnect();
 // express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(cors());
 
 // custom router middleware
 app.use("/v1", v1todo);
