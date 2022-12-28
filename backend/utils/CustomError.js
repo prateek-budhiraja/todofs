dcebed0af9e11d77b6d731755cb69fde0a4f7b9b
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2022-12-28 11:24:13 +0530

    modified exports to support commonjs

--- backend/utils/CustomError.js
+++ backend/utils/CustomError.js
@@ -1,13 +1,15 @@
-export class PropertyRequiredError extends Error {
+class PropertyRequiredError extends Error {
 	constructor(message) {
 		super("Invalid request **" + message + "** is required");
 		this.code = 403;
 	}
 }
 
-export class UnexpectedError extends Error {
+class UnexpectedError extends Error {
 	constructor(message) {
 		super("Something went wrong: " + message);
 		this.code = 503;
 	}
 }
+
+module.exports = { PropertyRequiredError, UnexpectedError };
