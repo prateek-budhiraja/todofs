46fc353680f3cf6a9ba6e2f4e3322d46a02eea48
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2022-12-29 22:05:09 +0530

    added components for the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-import CreateTodo from "./components/CreateTodo";
+import Heading from "./components/Heading";
 import ListTodo from "./components/ListTodo";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
 	return (
 		<div>
-			<h1>TODO app</h1>
-			<CreateTodo />
+			<Heading />
 			<ListTodo />
 		</div>
 	);
