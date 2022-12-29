e99ed91e80af38cc002e70e92fcf0172ecacee49
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2022-12-29 16:56:04 +0530

    added heading for the todo app

--- frontend/src/components/Heading.js
+++ frontend/src/components/Heading.js
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function Heading() {
-	return <h1>TODO app</h1>;
+	return (
+		<h1 className={"text-muted display-3 text-center mt-4 mb-5"}>TODO APP</h1>
+	);
 }
