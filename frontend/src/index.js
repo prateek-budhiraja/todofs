5bea03c9573b879fc4ac62aae20ae072ea38a7f5
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-01-02 18:52:58 +0530

    added routes for login, signup and root

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import axios from "axios";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 axios.defaults.baseURL = "http://localhost:4000/api";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+	<BrowserRouter>
+		<Routes>
+			<Route path="/login" element={<Login />} />
+			<Route path="/signup" element={<Signup />} />
+			<Route path="/" element={<App />} />s
+		</Routes>
+	</BrowserRouter>
 );
