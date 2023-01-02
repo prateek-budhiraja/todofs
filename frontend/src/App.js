b195a91047b4c5e968775cb15c747e76a7f27fcd
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-01-02 18:56:15 +0530

    added code to detect user login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,47 @@
 import Heading from "./components/Heading";
 import ListTodo from "./components/ListTodo";
-import Signup from "./components/Signup";
-import Login from "./components/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { account } from "./appwrite/appwrite.config";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+	const navigate = useNavigate();
+	const [userDetails, setUserDetails] = useState();
+
+	useEffect(() => {
+		const getUser = account.get();
+		getUser.then(
+			function (response) {
+				setUserDetails(response);
+			},
+			function (err) {
+				console.log(err);
+			}
+		);
+	}, []);
+
+	const logout = async () => {
+		try {
+			await account.deleteSession("current");
+			navigate("/login");
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Login />} />
-				<Route path="/signup" element={<Signup />} />
-			</Routes>
-			{/* <Heading />
-			<ListTodo /> */}
-		</BrowserRouter>
+		<>
+			<Heading />
+			{userDetails ? (
+				<ListTodo />
+			) : (
+				<h1 className="text-center mt-5">
+					Please <a href="/login">login</a> to view todos
+				</h1>
+			)}
+		</>
 	);
 }
 
