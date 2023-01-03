350bf23077da6f597d40a99c45a684d54873eee8
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-01-03 21:31:42 +0530

    added prehook on model and function to get token, compare password

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const JWT = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
 	name: {
@@ -16,7 +18,39 @@ const userSchema = mongoose.Schema({
 	password: {
 		type: String,
 		required: [true, "Password is required"],
+		select: false,
 	},
 });
+
+// encrypt the password when creating user
+userSchema.pre("save", async function (next) {
+	if (!this.isModified("password")) return next();
+	this.password = await bcrypt.hash(this.password, 10);
+	next();
+});
+
+// more functionality to user schema
+userSchema.methods = {
+	// compare encrypted password
+	comparePassword: async function (plainPassword) {
+		return await bcrypt.compare(plainPassword, this.password);
+	},
+
+	// generate jwt token
+	getJwtToken: async function () {
+		return JWT.sign(
+			{
+				_id: this._id,
+				email: this.email,
+			},
+			// config.JWT_SECRET,
+			"hello",
+			{
+				// expiresIn: config.JWT_EXPIRY,
+				expiresIn: "2h",
+			}
+		);
+	},
+};
 
 module.exports = mongoose.model("user", userSchema);
