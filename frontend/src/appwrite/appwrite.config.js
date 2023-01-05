b6e2b234135d715ce08db06113b3559a9797688c
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-01-05 11:49:41 +0530

    removed project id and added it to setup file

--- frontend/src/appwrite/appwrite.config.js
+++ frontend/src/appwrite/appwrite.config.js
@@ -1,7 +1,8 @@
-import { Client, Account, Databases } from "appwrite";
+import { Client, Account } from "appwrite";
+import setup from "./setup";
 
 const client = new Client();
 
-client.setEndpoint("http://localhost/v1").setProject("63b002241882beadcc0e");
+client.setEndpoint("http://localhost/v1").setProject(setup.PROJECT_ID);
 
 export const account = new Account(client);
